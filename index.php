26e54858b7cda980f75213122cbcb1bba768e0cb
verraca <veronica.racca@gmail.com>
2019-09-17 15:32:31 -0300

    Actualice el NavBar

--- index.php
+++ index.php
@@ -15,11 +15,13 @@
 <body>
 
 <header>
-<nav class="navbar navbar-expand-md navbar-light sticky-top" style="background-color: #20c997">
+<nav class="navbar navbar-expand-md navbar-light sticky-top" style="background-color: #dc3545">
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand font-weight-bold text-white" href="#">Service Nuñez</a>
+  
+  <a class="navbar-brand font-weight-normal text-white" href="#">
+      <img class="logo" src="images/logo.png" alt="logo">Service Nuñez</a>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
@@ -34,16 +36,22 @@
       </li>
  
     </ul>
-    <form class="form-inline my-2 my-lg-0 font-weight-bold">
-      <input class="form-control mr-sm-2" type="search" placeholder="Buscar productos" aria-label="Search">
-      <button class="btn btn-sm btn-light my-2 my-sm-0" type="submit">Buscar</button>
-    </form>
-  </div> 
-
-  <div class="img ">
+    <div class="img d-block d-md-none">
     <a href="" class="text-decoration-none"><img src="images/carrito.png" alt="Carrito"></a>  
     <a href="" class="text-decoration-none" data-toggle="modal" data-target="#exampleModalCenter"><img src="images/user.png" alt="Usuario"></a> 
   </div>
+  </div> 
+  <form class="form-inline my-2 my-lg-0 font-weight-bold d-none d-lg-block">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar productos" aria-label="Search">
+      <button class="btn btn-sm btn-light my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+  <div class="img d-none d-md-block">
+    <a href="" class="text-decoration-none"><img src="images/carrito.png" alt="Carrito"></a>  
+    <a href="" class="text-decoration-none" data-toggle="modal" data-target="#exampleModalCenter"><img src="images/user.png" alt="Usuario"></a> 
+  </div>
+  <span class="img">
+  <a href="" class="text-decoration-none d-block d-lg-none"><img src="images/busqueda.png" alt="Carrito"></a>  
+</span>
 </nav>
 </header>
 
