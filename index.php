363026c78f86f472682dea4ddc217d66ed7b1a94
verraca <veronica.racca@gmail.com>
2019-09-18 18:21:26 -0300

    Sume navegador.php y lo inclui en el index.php

--- index.php
+++ index.php
@@ -13,68 +13,10 @@
 
 <body>
 
-  <header>
-    <nav class="navbar navbar-expand-md navbar-light sticky-top" style="background-color: #dc3545">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<?php 
+include "./navegador.php";
+?>
 
-      <a class="navbar-brand font-weight-normal text-white" href="#">
-        <img class="logo" src="images/logo.png" alt="logo">Service Nuñez</a>
-
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Categorias</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contacto</a>
-          </li>
-
-        </ul>
-        <div class="img d-block d-md-none">
-          <a href="" class="text-decoration-none"><img src="images/carrito.png" alt="Carrito"></a>
-          <a href="" class="text-decoration-none" data-toggle="modal" data-target="#exampleModalCenter"><img src="images/user.png" alt="Usuario"></a>
-        </div>
-      </div>
-      <form class="form-inline my-2 my-lg-0 font-weight-bold d-none d-lg-block">
-        <input class="form-control mr-sm-2" type="search" placeholder="Buscar productos" aria-label="Search">
-        <button class="btn btn-sm btn-light my-2 my-sm-0" type="submit">Buscar</button>
-      </form>
-      <div class="img d-none d-md-block">
-        <a href="" class="text-decoration-none"><img src="images/carrito.png" alt="Carrito"></a>
-        <a href="" class="text-decoration-none" data-toggle="modal" data-target="#exampleModalCenter"><img src="images/user.png" alt="Usuario"></a>
-      </div>
-      <span class="img">
-        <a href="" class="text-decoration-none d-block d-lg-none"><img src="images/busqueda.png" alt="Carrito"></a>
-      </span>
-    </nav>
-  </header>
-
-
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalCenterTitle">Iniciar Sesion</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Salir</button>
-          <button type="button" class="btn btn-primary">Iniciar</button>
-        </div>
-      </div>
-    </div>
-  </div>
   <section>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
---
+++ navegador.php
@@ -0,0 +1,42 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-light sticky-top" style="background-color: #dc3545">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <a class="navbar-brand font-weight-normal text-white" href="#">
+        <img class="logo" src="images/logo.png" alt="logo">Service Nuñez</a>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Categorias</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li>
+
+        </ul>
+        <div class="img d-block d-md-none">
+          <a href="" class="text-decoration-none"><img src="images/carrito.png" alt="Carrito"></a>
+          <a href="" class="text-decoration-none" data-toggle="modal" data-target="#exampleModalCenter"><img src="images/user.png" alt="Usuario"></a>
+        </div>
+      </div>
+      <form class="form-inline my-2 my-lg-0 font-weight-bold d-none d-lg-block">
+        <input class="form-control mr-sm-2" type="search" placeholder="Buscar productos" aria-label="Search">
+        <button class="btn btn-sm btn-light my-2 my-sm-0" type="submit">Buscar</button>
+      </form>
+      <div class="img d-none d-md-block">
+        <a href="" class="text-decoration-none"><img src="images/carrito.png" alt="Carrito"></a>
+        <a href="" class="text-decoration-none" data-toggle="modal" data-target="#exampleModalCenter"><img src="images/user.png" alt="Usuario"></a>
+      </div>
+      <span class="img">
+        <a href="" class="text-decoration-none d-block d-lg-none"><img src="images/busqueda.png" alt="Carrito"></a>
+      </span>
+    </nav>
+  </header>
+
+
